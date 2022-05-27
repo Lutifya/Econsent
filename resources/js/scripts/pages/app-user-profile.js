e91dc6eca64c264060b1e46c3f510afa3cd95bf0
Stefano Quartuccio <s.quartuccio@elibra.eu>
2022-05-27 20:42:37 +0200

    Wip database change

--- resources/js/scripts/pages/app-user-profile.js
+++ resources/js/scripts/pages/app-user-profile.js
@@ -77,7 +77,7 @@ $(function () {
                         });
 
                         jQuery.ajax({
-                            url: assetPath + 'user/saveProfile/'+ $('#id').val(),
+                            url: assetPath + 'user/saveProfile/',
                             method: 'POST',
                             data: variabili,
                             success: function (result) {
